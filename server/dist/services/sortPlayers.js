7577098e8068ca2fad88d54c5acd18033d846246
Xtrazyx <xtrazyx@gmail.com>
2023-02-24 09:44:01 +0100

    fix: remove dist from git

--- server/dist/services/sortPlayers.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.sortPlayersById = void 0;
-function sortPlayersById(list) {
-    return list.sort((a, b) => Number(a.id) - Number(b.id));
-}
-exports.sortPlayersById = sortPlayersById;
-//# sourceMappingURL=sortPlayers.js.map
